5d8ac6e3c029f5888630272313c312701b88096f
meohin <meohin.meo@gmail.com>
2022-04-05 16:35:03 +0900

    Add application

---
+++ src/main/java/commentAdmin/CommentAdminApplication.java
@@ -0,0 +1,13 @@
+package commentAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/commentAdmin/CommentAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package commentAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
